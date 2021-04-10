235afaa7450f3cad05e37b398fcf6470875babc8
ekow-mensah <yamoah.ensah@yahoo.com>
2021-04-10 16:00:46 +0200

    major refactoring and design changes

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const {src, dest, watch} = require('gulp');
+const sass = require('gulp-sass');
+const minifyCss = require('gulp-csso');
+const concat = require('gulp-concat')
+
+function scss() {
+    return src('src/scss/**')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCss())
+    .pipe(concat('styles.css'))
+    .pipe(dest('public_html/css/style'));
+}
+
+if (process.env.NODE_ENV === 'development') {
+    watch('./src/scss/**/*.scss', scss);
+}
+
+exports.scss = scss;
+exports.default = scss;
